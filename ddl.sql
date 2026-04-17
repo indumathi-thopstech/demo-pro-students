42db99448c2e6005bf0b0fc2d75752a2a5b7552f
InduMathi <indumathithopstech@gmail.com>
2026-04-17 07:32:59 +0530

    adding only ddl first commit

---
+++ ddl.sql
@@ -0,0 +1,4 @@
+select * from table_name where condition;
+update table_name set column_name ="new_value" where condition;
+insert into table_name values(row1);
+delete from table_name where condition;
